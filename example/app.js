55a701b8cf2925763947a5bd93e8881ecae0ebf8
Hai Bacti <tranmhaii@gmail.com>
2019-01-01 15:23:59 +0700

    +

--- example/app.js
+++ example/app.js
@@ -1,74 +1,73 @@
-import '../lib/react-ui-tree.css';
-import './theme.css';
-import './app.css';
-import cx from 'classnames';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Tree from '../lib/react-ui-tree.js';
-import tree from './tree';
-import packageJSON from '../package.json';
+import '../lib/react-ui-tree.css'
+import './theme.css'
+import './app.css'
+import cx from 'classnames'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Tree from '../lib/react-ui-tree.js'
+import tree from './tree'
+import packageJSON from '../package.json'
 
-class App extends Component {
-  state = {
-    active: null,
-    tree: tree
-  };
+class App extends Component
+{
+    state =
+    {
+        active: null,
+        tree: tree
+    }
 
-  renderNode = node => {
-    return (
-      <span
-        className={cx('node', {
-          'is-active': node === this.state.active
-        })}
-        onClick={this.onClickNode.bind(null, node)}
-      >
-        {node.module}
-      </span>
-    );
-  };
+    renderNode = node =>
+    {
+        return (
+            <span
+                className={cx('node', { 'is-active': node === this.state.active })}
+                onClick={this.onClickNode.bind(null, node)}
+            >
+                {node.module}
+            </span>
+        )
+    }
 
-  onClickNode = node => {
-    this.setState({
-      active: node
-    });
-  };
+    onClickNode = node =>
+    {
+        this.setState({ active: node })
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <div className="tree">
-          <Tree
-            paddingLeft={20}
-            tree={this.state.tree}
-            onChange={this.handleChange}
-            isNodeCollapsed={this.isNodeCollapsed}
-            renderNode={this.renderNode}
-          />
-        </div>
-        <div className="inspector">
-          <h1>
-            {packageJSON.name} {packageJSON.version}
-          </h1>
-          <button onClick={this.updateTree}>update tree</button>
-          <pre>{JSON.stringify(this.state.tree, null, '  ')}</pre>
-        </div>
-      </div>
-    );
-  }
+    render()
+    {
+        return (
+            <div className="app">
+                <div className="tree">
+                    <Tree
+                        paddingLeft={20}
+                        tree={this.state.tree}
+                        onChange={this.handleChange}
+                        isNodeCollapsed={this.isNodeCollapsed}
+                        renderNode={this.renderNode}
+                    />
+                </div>
+                <div className="inspector">
+                    <h1>
+                        {packageJSON.name} {packageJSON.version}
+                    </h1>
+                    <button onClick={this.updateTree}>update tree</button>
+                    <pre>{JSON.stringify(this.state.tree, null, '    ')}</pre>
+                </div>
+            </div>
+        )
+    }
 
-  handleChange = tree => {
-    this.setState({
-      tree: tree
-    });
-  };
+    handleChange = tree =>
+    {
+        this.setState({ tree: tree })
+    }
 
-  updateTree = () => {
-    const { tree } = this.state;
-    tree.children.push({ module: 'test' });
-    this.setState({
-      tree: tree
-    });
-  };
+    updateTree = _ =>
+    {
+        const { tree } = this.state
+        tree.children.push({ module: 'test' })
+        this.setState({ tree: tree })
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
