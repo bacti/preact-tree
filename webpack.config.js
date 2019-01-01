cc7db8a9d49e4a40a7b5b91fca1cdcaae67863ea
Hai Bacti <tranmhaii@gmail.com>
2019-01-01 13:45:22 +0700

    +

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,83 @@
-module.exports = {
-  mode: 'development',
-  entry: './example/app.js',
-  output: {
-    path: __dirname + '/example',
-    filename: 'bundle.js',
-    publicPath: '/example/'
-  },
-  module: {
-    rules: [
-      { test: /\.m?js$/, exclude: /node_modules/, use: 'babel-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' }
-    ]
-  },
-  devtool: 'source-map'
-};
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+let options =
+{
+    entry: './js/main.js',
+    output:
+    {
+        path: path.resolve(__dirname, '.release'),
+        filename: 'main.js',
+        publicPath: process.env.NODE_ENV == 'production' ? './' : '/',
+    },
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.(js|jsx)(\?\S*)?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query:
+                {
+                    presets: ['es2015', 'react', 'stage-0'],
+                    plugins: ['transform-decorators-legacy'],
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{ loader: 'ifdef-loader', 
+                options:
+                {
+                    DEBUG: process.env.ADS_TYPE == 'DEBUG',
+                }
+                }]
+            },
+            {
+                test: /\.glsl$/,
+                loader: 'webpack-glsl-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    devServer: {
+        compress: true,
+        disableHostCheck: true,
+    },
+    plugins:
+    [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin(
+        {
+            template: 'index.html',
+            inject: 'body',
+        }),
+    ],
+    resolveLoader:
+    {
+        modules: [path.resolve('./node_modules'), process.env.NODE_PATH],
+    },
+    resolve:
+    {
+        modules: [path.resolve('./node_modules'), process.env.NODE_PATH],
+    },
+}
+if (process.env.NODE_ENV == 'production')
+{
+    options.plugins.push(new UglifyJSPlugin())
+}
+module.exports = options
